f6bc73c9888bcab2c2a0dc7a8680450a18b4c14e
Sebastián Kevin Escobar Briceño <sebastian.escobar@witi.cl>
2022-09-30 13:48:34 -0300

    Tab Navigation

--- App.js
+++ App.js
@@ -1,74 +1,71 @@
 import { StyleSheet, Text, View, Button } from 'react-native';
 import MapView from 'react-native-maps';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-
+import { NavigationContainer, TabActions } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native'
+import { Ionicons } from '@expo/vector-icons';
 import Ionic from 'react-native-vector-icons/Ionicons'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 
-export default function App() {
-
-  const[origin, set0rigin] = React.useState({
-    latitude: -33.046225180980755,
-    longitude: -71.62489754321773
-  })
-
-  function HomeScreen({ navigation }) {
+  const HomeScreen = () => {
+    const[origin, set0rigin] = React.useState({
+      latitude: -33.046225180980755,
+      longitude: -71.62489754321773
+    });
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      map: {
+        width:'100%',
+        height: '100%'
+      }
+    });
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => navigation.navigate('Details')}
+    <>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <MapView style={styles.map}
+          initialRegion={{
+          latitude: origin.latitude,
+          longitude: origin.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04
+          }}
         />
-      </View>
+    </View>
+    </>
     );
   }
-  function DetailsScreen({ navigation }) {
+
+  const SettingsScreen = () => {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      </View>
+    <>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+      {/* <Button title="Go to Home" onPress={() => navigation.navigate('Home')} /> */}
+    </View>
+    </>
+
     );
   }
 
-  const Stack = createNativeStackNavigator();  
+const Tab = createBottomTabNavigator();
 
+ const App = () => {
+  
   return (
-    <NavigationContainer>{
-      // <View style={styles.container}>
-      //   {/* <MapView style={styles.map}
-      //     initialRegion={{
-      //     latitude: origin.latitude,
-      //     longitude: origin.longitude,
-      //     latitudeDelta: 0.09,
-      //     longitudeDelta: 0.04
-      //     }}
-      //     /> */}
-      //     {/* <Text>Holi</Text>  */}
-
-          
-      // </View>  
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen}  options={{ title: 'Overview' }}/>
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    }</NavigationContainer>
-
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  </NavigationContainer>
   );
 }
+
+export default App;
 //comentario
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width:'100%',
-    height: '100%'
-  }
-});
