b98088e8d55059ab038a6401e589238b34563dbe
Sebastián Kevin Escobar Briceño <sebastian.escobar@witi.cl>
2022-09-29 23:48:27 -0300

    first commit

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import MapView from 'react-native-maps';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
+import Ionic from 'react-native-vector-icons/Ionicons'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 export default function App() {
@@ -12,20 +14,47 @@ export default function App() {
     longitude: -71.62489754321773
   })
 
+  function HomeScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+  function DetailsScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      </View>
+    );
+  }
+
+  const Stack = createNativeStackNavigator();  
 
   return (
     <NavigationContainer>{
-      <View style={styles.container}>
-        {/* <MapView style={styles.map}
-          initialRegion={{
-          latitude: origin.latitude,
-          longitude: origin.longitude,
-          latitudeDelta: 0.09,
-          longitudeDelta: 0.04
-          }}
-          /> */}
-          <Text>Holi</Text> 
-      </View>  
+      // <View style={styles.container}>
+      //   {/* <MapView style={styles.map}
+      //     initialRegion={{
+      //     latitude: origin.latitude,
+      //     longitude: origin.longitude,
+      //     latitudeDelta: 0.09,
+      //     longitudeDelta: 0.04
+      //     }}
+      //     /> */}
+      //     {/* <Text>Holi</Text>  */}
+
+          
+      // </View>  
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}  options={{ title: 'Overview' }}/>
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
     }</NavigationContainer>
 
   );
