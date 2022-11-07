60dde0186b572596e1f4bdbe71437629d8072002
charliboi <carlos.cubillos@witi.cl>
2022-11-07 20:42:53 -0300

    creacinnn login

--- App.js
+++ App.js
@@ -1,31 +1,10 @@
-import * as React from "react";
-import { NavigationContainer, TabActions } from "@react-navigation/native";
 
-import { Ionicons } from "@expo/vector-icons";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
-import ProfileScreen from "./pdt/screens/profile";
-import HomeScreen from "./pdt/screens/home";
-import SettingsScreen from "./pdt/screens/settings";
-import Login from "./pdt/screens/login";
+import React from 'react';
+import MyNavigation from './src/MyNavigation';
 
 const App = () => {
-  // const Stack = createNativeStackNavigator();
-  // const Tab = createBottomTabNavigator();
-  const Tab = createMaterialBottomTabNavigator();
-
-  return (
-    <>
-      <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} name="Login" component={Login} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-      </NavigationContainer>
-    </>
-  );
+  return <MyNavigation />;
 };
 
 export default App;
-//
